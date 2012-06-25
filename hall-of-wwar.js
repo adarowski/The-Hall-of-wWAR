78df15fcf8cf7e744da4b5bcde5d44bb6435318f
Adam Darowski <adarowski@patientslikeme.com>
2012-06-24 21:12:11 -0400

    Updated Hall of wWAR, based on Baseball-Reference's WAR (v 2.1).

--- hall-of-wwar.js
+++ hall-of-wwar.js
@@ -1,157 +1,115 @@
 $(document).ready(function(){
-  $("#list-in, #list-out, #list-next, #list-all").tablesorter();
-  $("#toggles dd#show-all").addClass("yah");
-  $("#timeline li div, #list-out, #timeline-out, #list-next, #timeline-next, nav ul").css("display","none");
+
+  function hideAll() {
+    $("#timeline li, #list-view tbody tr").hide();
+  }
+
+  hideAll();
+  $(".how2").show();
+  $(".sortable").tablesorter();
+  $("#toggles #show-all").addClass("yah");
+
+  $("#timeline li div, #visuals").hide();
   $("#timeline li").hover(function(){
     $(this).find('div').toggle();
   });
+
   $("#show-visuals").click(function(){
-    $("nav ul").toggle();
+    $("#visuals").toggle();
   });
+
   $("#show-all").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li").css("display","block"); // show all timeline-in boxes
-    $("#list-in tbody tr").css("display","table-row"); // show all list-in rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#show-p").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li, #list-in tbody tr").css("display","none"); // hide all rows and boxes
-    $("#timeline-in li.p, #timeline-in li.rp").css("display","block"); // show all p boxes
-    $("#list-in tbody tr.p, #list-in tbody tr.rp").css("display","table-row"); // show all p rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2.p, .how2.rp").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#show-pos").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li").css("display","block"); // show all timeline-in boxes
-    $("#list-in tbody tr").css("display","table-row"); // show all list-in rows
-    $("#timeline-in li.p, #timeline-in li.rp").css("display","none"); // hide all p boxes
-    $("#list-in tbody tr.p, #list-in tbody tr.rp").css("display","none"); // hide all p rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2").show();
+    $(".how2.p, .how2.rp").hide();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#show-c").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li, #list-in tbody tr").css("display","none"); // hide all rows and boxes
-    $("#timeline-in li.c").css("display","block"); // show all c boxes
-    $("#list-in tbody tr.c").css("display","table-row"); // show all c rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2.c").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#show-1b").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li, #list-in tbody tr").css("display","none"); // hide all rows and boxes
-    $("#timeline-in li.1b").css("display","block"); // show all 1b boxes
-    $("#list-in tbody tr.1b").css("display","table-row"); // show all 1b rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2.1b").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#show-2b").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li, #list-in tbody tr").css("display","none"); // hide all rows and boxes
-    $("#timeline-in li.2b").css("display","block"); // show all 2b boxes
-    $("#list-in tbody tr.2b").css("display","table-row"); // show all 2b rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2.2b").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#show-3b").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li, #list-in tbody tr").css("display","none"); // hide all rows and boxes
-    $("#timeline-in li.3b").css("display","block"); // show all 3b boxes
-    $("#list-in tbody tr.3b").css("display","table-row"); // show all 3b rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2.3b").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#show-ss").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li, #list-in tbody tr").css("display","none"); // hide all rows and boxes
-    $("#timeline-in li.ss").css("display","block"); // show all ss boxes
-    $("#list-in tbody tr.ss").css("display","table-row"); // show all ss rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2.ss").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#show-lf").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li, #list-in tbody tr").css("display","none"); // hide all rows and boxes
-    $("#timeline-in li.lf").css("display","block"); // show all lf boxes
-    $("#list-in tbody tr.lf").css("display","table-row"); // show all lf rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2.lf").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#show-cf").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li, #list-in tbody tr").css("display","none"); // hide all rows and boxes
-    $("#timeline-in li.cf").css("display","block"); // show all cf boxes
-    $("#list-in tbody tr.cf").css("display","table-row"); // show all cf rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2.cf").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#show-rf").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li, #list-in tbody tr").css("display","none"); // hide all rows and boxes
-    $("#timeline-in li.rf").css("display","block"); // show all rf boxes
-    $("#list-in tbody tr.rf").css("display","table-row"); // show all rf rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2.rf").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#show-dh").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li, #list-in tbody tr").css("display","none"); // hide all rows and boxes
-    $("#timeline-in li.dh").css("display","block"); // show all dh boxes
-    $("#list-in tbody tr.dh").css("display","table-row"); // show all dh rows
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2.dh").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#whos-in").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-in").css("display","block"); // show timeline-in
-    $("#list-in").css("display","table"); // show list-in
-    $("#timeline-in li").css("display","block"); // show all timeline-in boxes
-    $("#list-in tbody tr").css("display","table-row"); // show all list-in rows
-    $("#timeline-in li.hof").css("display","none"); // hide timeline-in boxes if they're in the hof
-    $("#list-in tr.hof").css("display","none"); // hide list-in rows if they're in the hof
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".how2").show();
+    $(".hof").hide();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#whos-out").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-out").css("display","block"); // show timeline-out
-    $("#list-out").css("display","table"); // show list-out
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".hof").show();
+    $(".how2").hide();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   $("#whos-next").click(function(){
-    $("#timeline ul, #list-view table").css("display","none"); // hide all timelines and tables
-    $("#timeline-next").css("display","block"); // show timeline-next
-    $("#list-next").css("display","table"); // show list-next
-    $("#toggles a").removeClass("yah"); // remove all yah classes from toggles
-    $(this).addClass("yah"); // add yah class to this toggle
+    hideAll();
+    $(".upcoming").show();
+    $("#toggles a").removeClass("yah");
+    $(this).addClass("yah");
   });
   if( window.location.hash ) {
     $(window.location.hash).click();
